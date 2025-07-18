36b983e127a1514780a8264ad7813650d1cf45fe
HaminLee-Thingswell <haminlee@thingswell.co.kr>
2025-07-18 16:11:08 +0900

    지도 삽입, 연락처 디자인 변경

--- grape/src/pages/About/About.jsx
+++ grape/src/pages/About/About.jsx
@@ -401,10 +401,7 @@ const About = () => {
                   </div>
                   <div className="location-container">
                     <div className="map-container">
-                      <OpenStreetMap
-                        address="경기 안양시 동안구 학의로 2825 금강펜타리움 IT타워 A동 1302호"
-                        companyName="(주)싱스웰"
-                      />
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.532835223222!2d126.96427059698114!3d37.40087886162995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b5e3a312a3d07%3A0x8362eaabc267ff81!2z6riI6rCV7Y6c7YWM66as7JuASVTtg4Dsm4w!5e0!3m2!1sko!2skr!4v1752822417888!5m2!1sko!2skr" width="600" height="450" style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                     <div className="location-info">
                       <div className="location-card">
