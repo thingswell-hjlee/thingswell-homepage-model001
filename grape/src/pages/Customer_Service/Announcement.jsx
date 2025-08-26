5918351d17d94bb5a95c6d1916aca21517336b32
HaminLee-Thingswell <haminlee@thingswell.co.kr>
2025-08-26 14:28:34 +0900

    .헤더 이미지 변경

--- grape/src/pages/Customer_Service/Announcement.jsx
+++ grape/src/pages/Customer_Service/Announcement.jsx
@@ -3,17 +3,22 @@ import Board from "../../components/Board";
 import { BaseLayout } from "../../components/Layout";
 import Breadcrumbs from "../../components/Breadcrumbs";
 import ProductHeader from "../../components/ProductPage/ProductHeader";
+import company from "../../assets/header_image/company.jpg";
 
 const Announcement = () => {
   return (
-    <BaseLayout
-      header={ProductHeader}
-      breadcrumbs={<Breadcrumbs breadcrumbs={["Home", "회사소개", "공지사항"]} />}
-      title="공지사항"
-      // subtitle="게시판을 확인하세요"
-    >
-      <Board tableNames={["Board_Announcement", "Board_Download"]} />
+    <div className="main-content">
+              <ProductHeader 
+          image={company} 
+          alt="company"
+          breadcrumbs={<Breadcrumbs breadcrumbs={["Home", "회사소개", "공지사항"]} />}
+          title="공지사항"
+        />
+    <BaseLayout>
+        <Board tableNames={["Board_Announcement", "Board_Download"]} />
+
     </BaseLayout>
+    </div>
   );
 };
 
