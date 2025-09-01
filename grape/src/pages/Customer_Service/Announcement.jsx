10b6996bbbf3634d934568a6f52adda19f7e1d87
HaminLee-Thingswell <haminlee@thingswell.co.kr>
2025-09-01 14:20:52 +0900

    게시판 반응형 디자인 적용

--- grape/src/pages/Customer_Service/Announcement.jsx
+++ grape/src/pages/Customer_Service/Announcement.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Board from "../../components/Board";
 import { BaseLayout } from "../../components/Layout";
-import Breadcrumbs from "../../components/Breadcrumbs";
 import ProductHeader from "../../components/ProductPage/ProductHeader";
 import company from "../../assets/header_image/company.jpg";
 
@@ -13,7 +12,7 @@ const Announcement = () => {
         alt="company"
       />
       <BaseLayout
-        breadcrumbs={<Breadcrumbs breadcrumbs={["Home", "고객지원", "공지사항"]} />}
+        breadcrumbs={["Home", "고객지원", "공지사항"]}
         title="공지사항"
       >
         <Board tableNames={["Board_Announcement", "Board_Download"]} />
